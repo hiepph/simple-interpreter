4b7729ded0f621c5dc51f8316d45cebdae99552a
Hiep Pham <hiepph.2406@gmail.com>
2020-07-15 00:10:23 +0700

    feat: Separate Lexer, Parser and Interpreter

--- calc.go
+++ calc.go
@@ -12,6 +12,7 @@ type tokenKind uint
 const (
 	numericKind tokenKind = iota
 	operatorKind
+	EOFKind
 )
 
 type Operator string
@@ -19,11 +20,15 @@ type Operator string
 const (
 	PLUS  Operator = "PLUS"
 	MINUS Operator = "MINUS"
+	MUL   Operator = "MUL"
+	DIV   Operator = "DIV"
 )
 
 var operatorDictionary = map[string]Operator{
 	"+": PLUS,
 	"-": MINUS,
+	"*": MUL,
+	"/": DIV,
 }
 
 type Token struct {
@@ -45,8 +50,7 @@ func isOperator(c byte) bool {
 	return found
 }
 
-func expr(text string) (int, error) {
-	// 1. lexing: decompose string into tokens
+func lex(text string) ([]Token, error) {
 	tokens := make([]Token, 0)
 	i := 0
 	for i < len(text) {
@@ -68,17 +72,19 @@ func expr(text string) (int, error) {
 				Token{operatorKind, string(c), -1})
 			i++
 		default:
-			return -1, errors.New("Error lexing input")
+			return tokens, errors.New("Error lexing input")
 		}
-
 	}
 
-	// 2. parser: convert tokens into values based on their kinds
+	return tokens, nil
+}
+
+func parse(tokens []Token) ([]Token, error) {
 	for i, token := range tokens {
 		if token.kind == numericKind {
 			value, err := strconv.Atoi(token.value)
 			if err != nil {
-				return -1, errors.New("Error parsing tokens")
+				return tokens, errors.New("Error parsing tokens")
 			}
 
 			tokens[i].intValue = value
@@ -88,28 +94,87 @@ func expr(text string) (int, error) {
 		}
 	}
 
-	// 3. interpreter: generate result
-	result := tokens[0].intValue
-	for i := 1; i < len(tokens); {
-		if tokens[i].kind == operatorKind {
-			switch tokens[i].value {
-			case "PLUS":
-				result += tokens[i+1].intValue
-			case "MINUS":
-				result -= tokens[i+1].intValue
-			}
+	return tokens, nil
+}
 
-			i += 2
-		} else {
-			i++
+/////// INTERPRETER
+type Interpreter struct {
+	tokens []Token
+	cur    int
+}
+
+func (itpr Interpreter) currentToken() Token {
+	if itpr.cur >= len(itpr.tokens) {
+		return Token{EOFKind, "", -1}
+	}
+	return itpr.tokens[itpr.cur]
+}
+
+func (itpr *Interpreter) eat(kind tokenKind) error {
+	if itpr.currentToken().kind == kind {
+		itpr.cur++
+		return nil
+	}
+
+	return errors.New("Error eating tokens")
+}
+
+func (itpr *Interpreter) factor() (int, error) {
+	token := itpr.currentToken()
+	err := itpr.eat(numericKind)
+	if err != nil {
+		return -1, err
+	}
+
+	return token.intValue, nil
+}
+
+func (itpr Interpreter) expr() (int, error) {
+	// expr: factor ((MUL|DIV)factor)*
+	// factor: INTEGER
+	result, err := itpr.factor()
+	if err != nil {
+		return -1, err
+	}
+
+	for itpr.currentToken().kind == operatorKind {
+		token := itpr.currentToken()
+		switch token.value {
+		case "MUL":
+			itpr.eat(operatorKind)
+			v, _ := itpr.factor()
+			result *= v
+		case "DIV":
+			itpr.eat(operatorKind)
+			v, _ := itpr.factor()
+			result /= v
 		}
 	}
 
 	return result, nil
 }
 
+///// main flow
+func interprete(text string) (int, error) {
+	// 1. lexing: decompose string into tokens
+	tokens, err := lex(text)
+	if err != nil {
+		return -1, err
+	}
+
+	// 2. parser: convert tokens into values based on their kinds
+	tokens, err = parse(tokens)
+	if err != nil {
+		return -1, err
+	}
+
+	// 3. interpreter: generate result
+	interpreter := Interpreter{tokens, 0}
+	return interpreter.expr()
+}
+
 func main() {
-	v, err := expr("13 + 5 - 12")
+	v, err := interprete("10 * 4  * 2 * 3 / 8")
 	if err != nil {
 		log.Fatal(err)
 	}
