eae4f78fad80427d17fb95019d343230c7a6db44
Hiep Pham <hiepph.2406@gmail.com>
2020-09-26 17:07:18 +0700

    feat: Pascal parser

--- si.go
+++ si.go
@@ -18,7 +18,22 @@ const (
 	assignKind
 	semiKind
 	dotKind
+	commaKind
+	colonKind
 )
+
+func (e tokenKind) String() string {
+	return [...]string{"numericKind",
+		"operatorKind",
+		"EOFKind",
+		"keywordKind",
+		"IDKind",
+		"assignKind",
+		"semiKind",
+		"dotKind",
+		"commaKind",
+		"colonKind"}[e]
+}
 
 type Operator string
 
@@ -50,6 +65,26 @@ type Token struct {
 }
 
 type AST interface {
+}
+
+type Program struct {
+	Name  string
+	Block Block
+}
+
+type Block struct {
+	Declarations      []VarDecl
+	CompoundStatement AST
+}
+
+type VarDecl struct {
+	VarNode  AST
+	TypeNode AST
+}
+
+type Type struct {
+	Token Token
+	Value interface{}
 }
 
 type BinOp struct {
@@ -150,13 +185,13 @@ func lex(text string) ([]Token, error) {
 				if err != nil {
 					return tokens, err
 				}
-				token = Token{numericKind, s, v}
+				token = Token{numericKind, "INTEGER_CONST", v}
 			case "REAL_CONST":
 				v, err := strconv.ParseFloat(s, 32)
 				if err != nil {
 					return tokens, err
 				}
-				token = Token{numericKind, s, v}
+				token = Token{numericKind, "REAL_CONST", v}
 			}
 			i = j - 1
 		case isSpace(c):
@@ -166,7 +201,8 @@ func lex(text string) ([]Token, error) {
 			j := i
 			for ; j < len(text) && text[j] != '}'; j++ {
 			}
-			i = j - 1
+			i = j + 1
+			continue
 		case isOperator(c):
 			op, _ := operatorDictionary[string(c)]
 			token = Token{operatorKind, string(op), -1}
@@ -177,8 +213,11 @@ func lex(text string) ([]Token, error) {
 			}
 			s := text[i:j]
 			if contains(keywordList, s) {
-				// TODO: check DIV
-				token = Token{keywordKind, s, -1}
+				if s == "DIV" {
+					token = Token{operatorKind, "DIV", -1}
+				} else {
+					token = Token{keywordKind, s, -1}
+				}
 			} else {
 				token = Token{IDKind, s, -1}
 			}
@@ -186,11 +225,16 @@ func lex(text string) ([]Token, error) {
 		case c == ':' && text[i+1] == '=':
 			token = Token{assignKind, ":=", -1}
 			i++
+		case c == ':' && text[i+1] != '=':
+			token = Token{colonKind, ":", -1}
 		case c == ';':
 			token = Token{semiKind, ";", -1}
 		case c == '.':
 			token = Token{dotKind, ".", -1}
+		case c == ',':
+			token = Token{commaKind, ",", -1}
 		default:
+			fmt.Println(string(c))
 			return tokens, errors.New("Error lexing input")
 		}
 
@@ -226,30 +270,50 @@ func (parser *Parser) eatOnlyKind(kind tokenKind) error {
 		parser.cur++
 		return nil
 	}
-	return errors.New(fmt.Sprintf("Error eating tokens %d."+
-		"want: %v, actual: %v", parser.cur, kind, token.Kind))
+	return errors.New(fmt.Sprintf("Error eating tokens %d: %v. "+
+		"want: %v, actual: %v", parser.cur, parser.tokens[parser.cur], kind, token.Kind))
 }
 
 func (parser *Parser) eat(kind tokenKind, value string) error {
 	token := parser.currentToken()
-	if (kind == numericKind && token.Kind == kind) ||
-		(token.Kind == kind && token.Value == value) {
+	if token.Kind == kind && token.Value == value {
 		parser.cur++
 		return nil
 	}
 
-	return errors.New(fmt.Sprintf("Error eating tokens %d."+
-		"want: %v, actual: %v", parser.cur, value, token.Value))
+	return errors.New(fmt.Sprintf("Error eating tokens %d: %v.\n"+
+		"> Kind, want: %v, actual: %v\n"+
+		"> Value, want: %v, actual: %v\n",
+		parser.cur, parser.tokens[parser.cur],
+		kind, token.Kind,
+		value, token.Value))
 }
 
 func (parser *Parser) program() (AST, error) {
-	// program: compoundStatement DOT
-	node, err := parser.compoundStatement()
+	// program: PROGRAM variable SEMI block DOT
+	err := parser.eat(keywordKind, "PROGRAM")
 	if err != nil {
 		return nil, err
 	}
+
+	varNode, err := parser.variable()
+	if err != nil {
+		return nil, err
+	}
+	progName := varNode.(Var).Value
+
+	err = parser.eatOnlyKind(semiKind)
+	blockNode, err := parser.block()
+	if err != nil {
+		return nil, err
+	}
+
+	programNode := Program{progName, blockNode.(Block)}
 	err = parser.eatOnlyKind(dotKind)
-	return node, err
+	if err != nil {
+		return nil, err
+	}
+	return programNode, nil
 }
 
 func (parser *Parser) compoundStatement() (AST, error) {
@@ -286,6 +350,9 @@ func (parser *Parser) statementList() ([]AST, error) {
 	results := []AST{node}
 	for parser.currentToken().Kind == semiKind {
 		err = parser.eatOnlyKind(semiKind)
+		if err != nil {
+			return nil, err
+		}
 		node, err := parser.statement()
 		if err != nil {
 			return nil, err
@@ -331,6 +398,7 @@ func (parser *Parser) assignStatement() (AST, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return Assign{Left: left, Op: token, Right: right}, nil
 }
 
@@ -348,21 +416,27 @@ func (parser *Parser) empty() (AST, error) {
 func (parser *Parser) factor() (AST, error) {
 	// factor : PLUS factor
 	//        | MINUS factor
-	//        | INTEGER
+	//        | INTEGER_CONST
+	//        | REAL_CONST
 	//        | LPAREN expr RPAREN
 	//        | variable
 	token := parser.currentToken()
 
+	var err error
 	switch token.Kind {
 	case numericKind:
-		err := parser.eat(numericKind, "INTEGER")
+		if token.Value == "INTEGER_CONST" {
+			err = parser.eat(numericKind, "INTEGER_CONST")
+		} else if token.Value == "REAL_CONST" {
+			err = parser.eat(numericKind, "REAL_CONST")
+		}
 		if err != nil {
 			return nil, err
 		}
 		return NewNum(token), nil
 	case operatorKind:
 		if token.Value == "LPAREN" {
-			err := parser.eat(operatorKind, "LPAREN")
+			err = parser.eat(operatorKind, "LPAREN")
 			if err != nil {
 				return nil, err
 			}
@@ -376,7 +450,7 @@ func (parser *Parser) factor() (AST, error) {
 			}
 			return node, nil
 		} else if token.Value == "PLUS" || token.Value == "MINUS" {
-			err := parser.eat(operatorKind, token.Value)
+			err = parser.eat(operatorKind, token.Value)
 			fact, err := parser.factor()
 			if err != nil {
 				return nil, err
@@ -392,26 +466,29 @@ func (parser *Parser) factor() (AST, error) {
 }
 
 func (parser *Parser) term() (AST, error) {
+	// term : factor ((MUL | DIV | FLOAT_DIV) factor)*
+	var err error
+
 	node, err := parser.factor()
 	if err != nil {
 		return nil, err
 	}
 
 	for parser.currentToken().Kind == operatorKind &&
-		contains([]string{"MUL", "DIV"}, parser.currentToken().Value) {
+		contains([]string{"MUL", "DIV", "FLOAT_DIV"}, parser.currentToken().Value) {
 		token := parser.currentToken()
 		switch token.Value {
 		case "MUL":
-			err := parser.eat(operatorKind, "MUL")
-			if err != nil {
-				return nil, err
-			}
+			err = parser.eat(operatorKind, "MUL")
 		case "DIV":
-			err := parser.eat(operatorKind, "DIV")
-			if err != nil {
-				return nil, err
-			}
+			err = parser.eat(operatorKind, "DIV")
+		case "FLOAT_DIV":
+			err = parser.eat(operatorKind, "FLOAT_DIV")
 		}
+		if err != nil {
+			return nil, err
+		}
+
 		rightNode, err := parser.factor()
 		if err != nil {
 			return nil, err
@@ -454,6 +531,104 @@ func (parser *Parser) expr() (AST, error) {
 	}
 
 	return node, nil
+}
+
+func (parser *Parser) block() (AST, error) {
+	// block: declarations compound_statement
+	declarationNodes, err := parser.declarations()
+	if err != nil {
+		return -1, err
+	}
+	compoundStatementNode, err := parser.compoundStatement()
+	if err != nil {
+		return -1, err
+	}
+	return Block{declarationNodes, compoundStatementNode}, nil
+}
+
+func (parser *Parser) declarations() ([]VarDecl, error) {
+	// declarations: VAR (variable_declaration SEMI)+
+	//             | empty
+	var result []VarDecl
+	currentToken := parser.currentToken()
+	if currentToken.Kind == keywordKind && currentToken.Value == "VAR" {
+		err := parser.eat(keywordKind, "VAR")
+		if err != nil {
+			return []VarDecl{}, err
+		}
+		for parser.currentToken().Kind == IDKind {
+			varDecls, err := parser.variableDeclarations()
+			if err != nil {
+				return []VarDecl{}, err
+			}
+			for _, varDecl := range varDecls {
+				result = append(result, varDecl)
+			}
+			err = parser.eatOnlyKind(semiKind)
+			if err != nil {
+				return []VarDecl{}, err
+			}
+		}
+	}
+
+	return result, nil
+}
+
+func (parser *Parser) variableDeclarations() ([]VarDecl, error) {
+	// variable_declaration: ID (COMMA ID)* COLON type_spec
+	varNodes := []AST{NewVar(parser.currentToken())}
+	err := parser.eatOnlyKind(IDKind)
+	if err != nil {
+		return []VarDecl{}, err
+	}
+
+	for parser.currentToken().Kind == commaKind {
+		err := parser.eatOnlyKind(commaKind)
+		if err != nil {
+			return []VarDecl{}, err
+		}
+		varNodes = append(varNodes, NewVar(parser.currentToken()))
+		err = parser.eatOnlyKind(IDKind)
+		if err != nil {
+			return []VarDecl{}, err
+		}
+	}
+
+	err = parser.eatOnlyKind(colonKind)
+	if err != nil {
+		return []VarDecl{}, err
+	}
+
+	typeNode, err := parser.typeSpec()
+	if err != nil {
+		return []VarDecl{}, err
+	}
+
+	var result []VarDecl
+	for _, varNode := range varNodes {
+		result = append(result, VarDecl{varNode, typeNode})
+	}
+
+	return result, nil
+}
+
+func (parser *Parser) typeSpec() (Type, error) {
+	token := parser.currentToken()
+	if token.Kind == keywordKind && token.Value == "INTEGER" {
+		err := parser.eat(keywordKind, "INTEGER")
+		if err != nil {
+			return Type{}, err
+		}
+	} else if token.Kind == keywordKind && token.Value == "REAL" {
+		err := parser.eat(keywordKind, "REAL")
+		if err != nil {
+			return Type{}, err
+		}
+	} else {
+		return Type{}, errors.New(fmt.Sprintf("Error checking type, tokens %d: %v.", parser.cur, token))
+	}
+
+	return Type{token, token.NumericValue}, nil
 }
 
 func (parser *Parser) parse() (AST, error) {
@@ -578,14 +753,15 @@ func do(text string) (interface{}, error) {
 	if err != nil {
 		return -1, err
 	}
-	return tokens, nil
+	fmt.Println(tokens)
 
 	// 2. parser: build AST representation
-	// parser := NewParser(tokens)
-	// node, err := parser.parse()
-	// if err != nil {
-	// 	return -1, err
-	// }
+	parser := NewParser(tokens)
+	node, err := parser.parse()
+	if err != nil {
+		return -1, err
+	}
+	return node, nil
 
 	// // 3. interpreter: generate result
 	// itpr := Interpreter{node: node, globalScope: make(map[string]int)}
@@ -593,35 +769,20 @@ func do(text string) (interface{}, error) {
 	// return itpr.globalScope, nil
 }
 
-// func main() {
-// 	// node, err := interprete("7 + 3 * (10 / (12 / (3 + 1) - 1))")
-// 	// node, err := interprete("2 * 7 + 3")
-// 	node, err := interprete("5-+-2")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Printf("%+v\n", node)
-
-// 	v, err := visit(node)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println(v)
-// }
-
 func main() {
 	// v, err := do("BEGIN a := 2; END.")
-	v, err := do(`BEGIN
-	    BEGIN
-	        number := 2;
-	        a := number;
-	        b := 10 * a + 10 * number / 4;
-	        c := a - - b
-	    END;
-	    x := 11;
-	END.`)
+	v, err := do(`PROGRAM Part10AST;
+VAR
+   a, b : INTEGER;
+   y    : REAL;
+
+BEGIN {Part10AST}
+   a := 2;
+   b := 10 * a + 10 * a DIV 4;
+   y := 20 / 7 + 3.14;
+END. {Part10AST}`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(v)
+	fmt.Printf("%+v\n", v)
 }
