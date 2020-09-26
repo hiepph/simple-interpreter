1effac6fa3dfc6f2628aa728875f8ae7cb15f89b
Hiep Pham <hiepph.2406@gmail.com>
2020-09-26 22:17:23 +0700

    feat: Pascal interpreter

--- si.go
+++ si.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"reflect"
 	"strconv"
 )
 
@@ -537,11 +538,11 @@ func (parser *Parser) block() (AST, error) {
 	// block: declarations compound_statement
 	declarationNodes, err := parser.declarations()
 	if err != nil {
-		return -1, err
+		return nil, err
 	}
 	compoundStatementNode, err := parser.compoundStatement()
 	if err != nil {
-		return -1, err
+		return nil, err
 	}
 	return Block{declarationNodes, compoundStatementNode}, nil
 }
@@ -638,43 +639,98 @@ func (parser *Parser) parse() (AST, error) {
 /////// INTERPRETER
 type Interpreter struct {
 	node        AST
-	globalScope map[string]int
+	globalScope map[string]interface{}
 }
 
-func (itpr *Interpreter) visitBinOp(node AST) (int, error) {
+func (itpr *Interpreter) visitBinOp(node AST) (interface{}, error) {
 	nodeBinOp := node.(BinOp)
 
 	left, err := itpr.visit(nodeBinOp.Left)
 	if err != nil {
-		return -1, err
+		return nil, err
 	}
 	right, err := itpr.visit(nodeBinOp.Right)
 	if err != nil {
-		return -1, err
+		return nil, err
 	}
 
 	switch nodeBinOp.Op.Value {
 	case "PLUS":
-		return left + right, nil
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return left.(int) + right.(int), nil
+		}
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Float64 {
+			return float64(left.(int)) + right.(float64), nil
+		}
+		if reflect.TypeOf(left).Kind() == reflect.Float64 &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return left.(float64) + float64(right.(int)), nil
+		}
+		return left.(float64) + right.(float64), nil
 	case "MINUS":
-		return left - right, nil
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return left.(int) - right.(int), nil
+		}
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Float64 {
+			return float64(left.(int)) - right.(float64), nil
+		}
+		if reflect.TypeOf(left).Kind() == reflect.Float64 &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return left.(float64) - float64(right.(int)), nil
+		}
+		return left.(float64) - right.(float64), nil
 	case "MUL":
-		return left * right, nil
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return left.(int) * right.(int), nil
+		}
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Float64 {
+			return float64(left.(int)) * right.(float64), nil
+		}
+		if reflect.TypeOf(left).Kind() == reflect.Float64 &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return left.(float64) * float64(right.(int)), nil
+		}
+		return left.(float64) * right.(float64), nil
 	case "DIV":
-		return left / right, nil
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return left.(int) / right.(int), nil
+		} else {
+			return nil, errors.New("Cannot DIV for float values")
+		}
+	case "FLOAT_DIV":
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return float64(left.(int)) / float64(right.(int)), nil
+		}
+		if reflect.TypeOf(left).Kind() == reflect.Int &&
+			reflect.TypeOf(right).Kind() == reflect.Float64 {
+			return float64(left.(int)) / right.(float64), nil
+		}
+		if reflect.TypeOf(left).Kind() == reflect.Float64 &&
+			reflect.TypeOf(right).Kind() == reflect.Int {
+			return left.(float64) / float64(right.(int)), nil
+		}
+		return left.(float64) / right.(float64), nil
 	default:
-		return -1, errors.New("Unkown Op")
+		return nil, errors.New("Unkown Op")
 	}
 }
 
-func (itpr *Interpreter) visitNum(node AST) (int, error) {
-	return node.(Num).Value.(int), nil
+func (itpr *Interpreter) visitNum(node AST) (interface{}, error) {
+	return node.(Num).Value, nil
 }
 
-func (itpr *Interpreter) visitUnaryOp(node AST) (int, error) {
+func (itpr *Interpreter) visitUnaryOp(node AST) (interface{}, error) {
 	n, ok := node.(UnaryOp)
 	if !ok {
-		return -1, errors.New("Error Unary Op")
+		return nil, errors.New("Error Unary Op")
 	}
 	switch n.Op.Value {
 	case "PLUS":
@@ -682,69 +738,72 @@ func (itpr *Interpreter) visitUnaryOp(node AST) (int, error) {
 	case "MINUS":
 		v, err := itpr.visit(n.expr)
 		if err != nil {
-			return -1, err
+			return nil, err
 		}
-		return -v, err
+		decimalV, ok := v.(int)
+		if ok {
+			return -decimalV, err
+		} else {
+			return -v.(float64), err
+		}
 	}
-	return -1, errors.New("Error visiting Unary Op")
+	return nil, errors.New("Error visiting Unary Op")
 }
 
-func (itpr *Interpreter) visitCompound(node AST) (int, error) {
+func (itpr *Interpreter) visitCompound(node AST) (interface{}, error) {
 	for _, child := range node.(Compound).Children {
 		_, err := itpr.visit(child)
 		if err != nil {
-			return -1, err
+			return nil, err
 		}
 	}
 
-	return -1, nil
+	return nil, nil
 }
 
-func (itpr *Interpreter) visitNoOp(node AST) (int, error) {
-	return -1, nil
+func (itpr *Interpreter) visitNoOp(node AST) (interface{}, error) {
+	return nil, nil
 }
 
-func (itpr *Interpreter) visitAssign(node AST) (int, error) {
+func (itpr *Interpreter) visitAssign(node AST) (interface{}, error) {
 	varName := node.(Assign).Left.(Var).Value
 	v, err := itpr.visit(node.(Assign).Right)
 	itpr.globalScope[varName] = v
-	return -1, err
+	return nil, err
 }
 
-func (itpr *Interpreter) visitVar(node AST) (int, error) {
+func (itpr *Interpreter) visitVar(node AST) (interface{}, error) {
 	varName := node.(Var).Value
 	val, ok := itpr.globalScope[varName]
 	if !ok {
-		return -1, errors.New(fmt.Sprintf("No %v registered\n", varName))
+		return nil, errors.New(fmt.Sprintf("No %v registered\n", varName))
 	}
 	return val, nil
 }
 
-func (itpr *Interpreter) visitProgram(node AST) error {
-	_, err := itpr.visit(node.(Program).Block)
-	return err
+func (itpr *Interpreter) visitProgram(node AST) (interface{}, error) {
+	return itpr.visit(node.(Program).Block)
 }
 
-func (itpr *Interpreter) visitBlock(node AST) error {
+func (itpr *Interpreter) visitBlock(node AST) (interface{}, error) {
 	for _, dec := range node.(Block).Declarations {
 		_, err := itpr.visit(dec)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
-	_, err := itpr.visit(node.(Block).CompoundStatement)
-	return err
+	return itpr.visit(node.(Block).CompoundStatement)
 }
 
-func (itpr *Interpreter) visitVarDecl(node AST) error {
-	return nil
+func (itpr *Interpreter) visitVarDecl(node AST) (interface{}, error) {
+	return nil, nil
 }
 
-func (itpr *Interpreter) visitType(node AST) error {
-	return nil
+func (itpr *Interpreter) visitType(node AST) (interface{}, error) {
+	return nil, nil
 }
 
-func (itpr *Interpreter) visit(node AST) (int, error) {
+func (itpr *Interpreter) visit(node AST) (interface{}, error) {
 	switch node.(type) {
 	case BinOp:
 		return itpr.visitBinOp(node)
@@ -760,12 +819,20 @@ func (itpr *Interpreter) visit(node AST) (int, error) {
 		return itpr.visitVar(node)
 	case Assign:
 		return itpr.visitAssign(node)
+	case Program:
+		return itpr.visitProgram(node)
+	case Block:
+		return itpr.visitBlock(node)
+	case VarDecl:
+		return itpr.visitVarDecl(node)
+	case Type:
+		return itpr.visitType(node)
 	default:
-		return -1, errors.New("Unknown node type")
+		return nil, errors.New("Unknown node type")
 	}
 }
 
-func (itpr Interpreter) interprete() (int, error) {
+func (itpr Interpreter) interprete() (interface{}, error) {
 	return itpr.visit(itpr.node)
 }
 
@@ -775,36 +842,63 @@ func do(text string) (interface{}, error) {
 	// also convert tokens into values based on their kinds
 	tokens, err := lex(text)
 	if err != nil {
-		return -1, err
+		return nil, err
 	}
-	fmt.Println(tokens)
+	// fmt.Println(tokens)
 
 	// 2. parser: build AST representation
 	parser := NewParser(tokens)
 	node, err := parser.parse()
 	if err != nil {
-		return -1, err
+		return nil, err
 	}
-	return node, nil
+	// fmt.Printf("%+v\n", node)
 
 	// // 3. interpreter: generate result
-	// itpr := Interpreter{node: node, globalScope: make(map[string]int)}
-	// itpr.interprete()
-	// return itpr.globalScope, nil
+	itpr := Interpreter{node: node, globalScope: make(map[string]interface{})}
+	_, err = itpr.interprete()
+	if err != nil {
+		return nil, err
+	}
+	return itpr.globalScope, nil
 }
 
 func main() {
 	// v, err := do("BEGIN a := 2; END.")
-	v, err := do(`PROGRAM Part10AST;
-VAR
-   a, b : INTEGER;
-   y    : REAL;
+	// 	v, err := do(`PROGRAM Part10AST;
+	// VAR
+	//    a, b : INTEGER;
+	//    y    : REAL;
 
-BEGIN {Part10AST}
-   a := 2;
-   b := 10 * a + 10 * a DIV 4;
+	// BEGIN {Part10AST}
+	//    a := 2;
+	//    b := 10 * a + 10 * a DIV 4;
+	//    y := 20 / 7 + 3.14;
+	// END. {Part10AST}`)
+	v, err := do(`
+PROGRAM Part10;
+VAR
+   number     : INTEGER;
+   a, b, c, x : INTEGER;
+   y          : REAL;
+
+BEGIN {Part10}
+   BEGIN
+      number := 2;
+      a := number;
+      b := 10 * a + 10 * number DIV 4;
+      c := a - - b
+   END;
+   x := 11;
    y := 20 / 7 + 3.14;
-END. {Part10AST}`)
+   { writeln('a = ', a); }
+   { writeln('b = ', b); }
+   { writeln('c = ', c); }
+   { writeln('number = ', number); }
+   { writeln('x = ', x); }
+   { writeln('y = ', y); }
+END.  {Part10}
+`)
 	if err != nil {
 		log.Fatal(err)
 	}
