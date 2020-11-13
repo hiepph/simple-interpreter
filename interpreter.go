8448de83e786817c9d82d347adae12acc9b38446
Hiep <hiepph.2406@gmail.com>
2020-11-13 18:25:55 +0700

    feat: integrate CallStack to Interpreter

--- interpreter.go
+++ interpreter.go
@@ -3,24 +3,25 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"reflect"
 )
 
 type Stack struct {
-	Items []interface{}
+	Items []ActivationRecord
 }
 
-func (s *Stack) push(item interface{}) {
+func (s *Stack) push(item ActivationRecord) {
 	s.Items = append(s.Items, item)
 }
 
-func (s *Stack) pop() interface{} {
+func (s *Stack) pop() ActivationRecord {
 	var lastItem = s.Items[len(s.Items)-1]
 	s.Items = s.Items[:len(s.Items)-1]
 	return lastItem
 }
 
-func (s Stack) peek() interface{} {
+func (s Stack) peek() ActivationRecord {
 	return s.Items[len(s.Items)-1]
 }
 
@@ -32,7 +33,7 @@ const (
 
 type ActivationRecord struct {
 	Name         string
-	Type         string
+	Type         ARType
 	NestingLevel int
 	Members      map[string]interface{}
 }
@@ -46,8 +47,8 @@ func (ar ActivationRecord) get(k string) interface{} {
 }
 
 type Interpreter struct {
-	node        AST
-	globalScope map[string]interface{}
+	node      AST
+	CallStack Stack
 }
 
 func (itpr *Interpreter) visitBinOp(node AST) (interface{}, error) {
@@ -63,7 +64,7 @@ func (itpr *Interpreter) visitBinOp(node AST) (interface{}, error) {
 	}
 
 	switch nodeBinOp.Op.Value {
-	case "PLUS":
+	case "+":
 		if reflect.TypeOf(left).Kind() == reflect.Int &&
 			reflect.TypeOf(right).Kind() == reflect.Int {
 			return left.(int) + right.(int), nil
@@ -77,7 +78,7 @@ func (itpr *Interpreter) visitBinOp(node AST) (interface{}, error) {
 			return left.(float64) + float64(right.(int)), nil
 		}
 		return left.(float64) + right.(float64), nil
-	case "MINUS":
+	case "-":
 		if reflect.TypeOf(left).Kind() == reflect.Int &&
 			reflect.TypeOf(right).Kind() == reflect.Int {
 			return left.(int) - right.(int), nil
@@ -91,7 +92,7 @@ func (itpr *Interpreter) visitBinOp(node AST) (interface{}, error) {
 			return left.(float64) - float64(right.(int)), nil
 		}
 		return left.(float64) - right.(float64), nil
-	case "MUL":
+	case "*":
 		if reflect.TypeOf(left).Kind() == reflect.Int &&
 			reflect.TypeOf(right).Kind() == reflect.Int {
 			return left.(int) * right.(int), nil
@@ -112,7 +113,7 @@ func (itpr *Interpreter) visitBinOp(node AST) (interface{}, error) {
 		} else {
 			return nil, errors.New("Cannot DIV for float values")
 		}
-	case "FLOAT_DIV":
+	case "/":
 		if reflect.TypeOf(left).Kind() == reflect.Int &&
 			reflect.TypeOf(right).Kind() == reflect.Int {
 			return float64(left.(int)) / float64(right.(int)), nil
@@ -127,7 +128,7 @@ func (itpr *Interpreter) visitBinOp(node AST) (interface{}, error) {
 		}
 		return left.(float64) / right.(float64), nil
 	default:
-		return nil, errors.New("Unknown Op")
+		return nil, errors.New(fmt.Sprintf("Unknown Op %s", nodeBinOp.Op.Value))
 	}
 }
 
@@ -176,21 +177,34 @@ func (itpr *Interpreter) visitNoOp(node AST) (interface{}, error) {
 func (itpr *Interpreter) visitAssign(node AST) (interface{}, error) {
 	varName := node.(Assign).Left.(Var).Value
 	v, err := itpr.visit(node.(Assign).Right)
-	itpr.globalScope[varName] = v
+	ar := itpr.CallStack.peek()
+	ar.set(varName, v)
 	return nil, err
 }
 
 func (itpr *Interpreter) visitVar(node AST) (interface{}, error) {
 	varName := node.(Var).Value
-	val, ok := itpr.globalScope[varName]
-	if !ok {
-		return nil, errors.New(fmt.Sprintf("No %v registered\n", varName))
-	}
+	ar := itpr.CallStack.peek()
+	val := ar.get(varName)
 	return val, nil
 }
 
 func (itpr *Interpreter) visitProgram(node AST) (interface{}, error) {
-	return itpr.visit(node.(Program).Block)
+	programName := node.(Program).Name
+
+	log.Printf("ENTER: PROGRAM %s\n", programName)
+	ar := ActivationRecord{Name: programName, Type: ProgramType, NestingLevel: 1, Members: make(map[string]interface{})}
+	itpr.CallStack.push(ar)
+	log.Println(ar)
+	_, err := itpr.visit(node.(Program).Block)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("LEAVE: PROGRAM %s\n", programName)
+	log.Println(ar)
+	itpr.CallStack.pop()
+
+	return nil, nil
 }
 
 func (itpr *Interpreter) visitBlock(node AST) (interface{}, error) {
